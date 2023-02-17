fb35bb480a136f0c94ee49ea3f755980f64212a9
Shivansh Kumar Dubey <shivanshdubey232@gmail.com>
2023-02-17 23:07:58 +0530

    Addition of index.html and app.js
    Addition of index.html and app.js

---
+++ app.js
@@ -0,0 +1,42 @@
+fetch("https://api.coinlore.net/api/tickers/")
+    .then((data) =>{
+        console.log("Here is your data : ", data)
+        return data.json()
+    })
+    .then((complete) => {
+        console.log(complete.data[0])
+        for(let i = 0; i < 100; i++)
+        {
+            let currencies = document.querySelector("#currencies") 
+            let currency = document.createElement("tr")
+            let currRank = document.createElement("td")
+            let currSymbol = document.createElement("td")
+            let currName = document.createElement("td")
+            let currPrice = document.createElement("td")
+            let currHour = document.createElement("td")
+            let currDay = document.createElement("td")
+            let currWeek = document.createElement("td")
+            currName.innerText = complete.data[i].name
+            currPrice.innerText = complete.data[i].price_usd
+            currRank.innerText = complete.data[i].rank
+            currSymbol.innerText = complete.data[i].symbol
+            currHour.innerText = complete.data[i].percent_change_1h
+            currDay.innerText = complete.data[i].percent_change_24h
+            currWeek.innerText = complete.data[i].percent_change_7d
+            currency.append(currName, currPrice, currRank, currSymbol, currName, currPrice, currHour, currDay, currWeek)
+            if(parseInt(complete.data[i].percent_change_7d)  > 0)
+            {
+
+                currency.classList.add("table-success")
+            }
+            else
+            {
+                currency.classList.add("table-danger")
+            }
+            currencies.append(currency)
+        }
+
+    })
+    .catch((e) => {
+        console.log("Oh no!, Error", e)
+    })
